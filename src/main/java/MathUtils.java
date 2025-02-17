8cd17445faca3e404b3336b439540c17ab2423f8
Atanu <mondalatanu208@gmail.com>
2025-02-17 12:57:48 +0530

    17-02-2025 12:57 PM

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -4,4 +4,18 @@ public class MathUtils {
     {
         return a+b;
     }
+
+    public double computeCircleArea(int radious)
+    {
+        return 3.14 * radious * radious;
+    }
+
+    public double devide(int numerator,int denominator)
+    {
+        return numerator/denominator;
+    }
+    int sub(int numerator,int denominator)
+    {
+        return numerator-denominator;
+    }
 }
--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,10 +1,36 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 
 class MathUtilsTest {
+    MathUtils obj;
+    @BeforeEach
+    void init()
+    {
+        obj=new MathUtils();
+    }
     @Test
     void TestAddMethod() {
-        MathUtils obj=new MathUtils();
         Assertions.assertEquals(40,obj.add(10,30));
+    }
+    @Test
+    void TestComputeCircleArea()
+    {
+        Assertions.assertEquals(314.0,obj.computeCircleArea(10));
+    }
+
+    @Test
+    void TestDevide()
+    {
+        assumeTrue(true);
+        Assertions.assertThrows(ArithmeticException.class, () -> obj.devide(10,0));
+    }
+    @DisplayName("Check sub method")
+    @Disabled
+    @Test
+    void TestSub()
+    {
+        Assertions.assertEquals(10,obj.sub(20,10));
     }
 }
